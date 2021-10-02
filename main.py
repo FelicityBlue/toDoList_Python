c08dd27aa649620320369389d07c29dc21e5b2bc
FelicityBlue <73523510+FelicityBlue@users.noreply.github.com>
2021-10-01 23:20:11 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,50 @@
+import toDoList
+
+user_choice = ''
+taskList = []
+
+
+# print all the choices user can make
+def print_action():
+    print("1. add item to the list")
+    print("2. check off the item")
+    print("3. Display the list")
+    print("4. exit the program")
+
+
+while user_choice != str(4):
+    print_action()
+    user_choice = input("Enter the index of the action :: ")
+    while not user_choice.isdigit():
+        print("Enter an integer.")
+        user_choice = input("Enter the index of the action :: ")
+
+    if user_choice == str(1):
+        choice = 'y'
+        while choice == "y":
+            # ask for the name of the task
+            itemName = input("Enter the task name:: ")
+            taskList.append(toDoList.add(itemName))
+
+            # ask if the user want to add another item
+            choice = input("Do you want to add an another item (y/n)? ")
+            choice.replace(" ", "")
+
+            # If the input is not valid then ask for it again
+            while choice != 'y' and choice != "n":
+                print("Enter a valid answer.")
+                choice = input("Do you want to add an another item (y/n)? ")
+                choice.replace(" ", "")
+        print()
+
+    elif user_choice == str(2):
+        toDoList.check_off(taskList)
+
+    elif user_choice == str(3):
+
+        toDoList.display_list(taskList)
+
+    elif user_choice != str(4):
+        print("Enter a valid choice.\n")
+
+print("Thank you for using the program\nHave a great day!")
---
+++ toDoList.py
@@ -0,0 +1,40 @@
+# add items to the list
+def add(task_name):
+    # add item to the list
+    # assign false as task done by default
+
+    return [task_name, False]
+
+
+def check_off(task_list):
+    display_list(task_list)
+    taskIndex = int(input("Enter the index of the item to check off:: "))
+    # if they choose the item that is already checked
+    if task_list[taskIndex - 1][1]:
+        print("It was already checkoff.")
+    else:
+        print("Item #" + str(taskIndex) + " checkoff.")
+        task_list[taskIndex - 1][1] = True
+
+    userChoice = input("Do you want to input another index (y/n)? ")
+    while userChoice != 'y' and userChoice != "n":
+        print("Enter a valid answer.")
+        userChoice = input("Do you want to input another index (y/n)? ")
+
+    if userChoice == 'y':
+        check_off(task_list)
+
+
+def display_list(task_list):
+    print("-To Do List-")
+    if len(task_list) == 0:
+        print("There is no task in the list.")
+    else:
+
+        for i in range(len(task_list)):
+            if task_list[i][1]:
+                print(str(i + 1) + ". [V] ", end='')
+            else:
+                print(str(i + 1) + '. [X] ', end='')
+            print(task_list[i][0])
+    print()
